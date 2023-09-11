2285d9114eaf53bfc5274cfb411b29aaea64caa2
gabrieladel <gabi23@live.com.ar>
2023-09-10 23:26:24 -0300

    contacto

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Inicio from './pages/Home';
 import Peliculas from './pages/Peliculas.jsx';
-import Contacto from './pages/Contacto';
+import Contacto from './pages/Contacto.jsx';
 
 function App() {
   return (
--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,34 +1,41 @@
 import React from "react";
-import { MDBInput, MDBCheckbox, MDBBtn } from 'mdb-react-ui-kit';
-import '../App.css'
+import '../App.css';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
 
-function Contacto () {
-    return (
-        <>
-        <MDBValidation noValidate id='form' className='text-center' style={{ width: '100%', maxWidth: '300px' }}>
-        <h2>Contacto</h2>
-  
-        <MDBValidationItem invalid feedback='Ingrese su nombre.'>
-          <MDBInput label='Name' v-model='name' wrapperClass='mb-4' required />
-        </MDBValidationItem>
-  
-        <MDBValidationItem invalid feedback='Igrese su email.'>
-          <MDBInput type='email' label='Email address' v-model='email' wrapperClass='mb-4' required />
-        </MDBValidationItem>
-  
-        <MDBValidationItem invalid feedback='Ingrese su consulta.'>
-          <MDBTextArea wrapperClass='mb-4' label='Message' required />
-        </MDBValidationItem>
-  
-        <MDBValidationItem feedback=''>
-          <MDBCheckbox wrapperClass='d-flex justify-content-center' label='Send me copy' />
-        </MDBValidationItem>
-  
-        <MDBBtn type='submit' color='primary' block className='my-4'>
-          Enviar
-        </MDBBtn>
-      </MDBValidation>
-         </>
-    );
+function Contacto() {
+  return (
+    <Form>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridName">
+          <Form.Label>Nombre</Form.Label>
+          <Form.Control type="name" placeholder="Nombre"/>
+        </Form.Group>
+
+      </Row>
+      <Row className="mb-3 ">
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Mensajes</Form.Label>
+        <Form.Control placeholder="text" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress">
+        <Form.Label>Dirección</Form.Label>
+        <Form.Control placeholder="Dirección" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Enviar
+      </Button>
+    </Form>
+  );
 }
 export default Contacto;
